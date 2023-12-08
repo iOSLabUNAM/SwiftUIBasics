4d4da942d91926a980f93aa883efcd7b256b7953
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2023-12-08 17:34:08 -0600

    Initial Commit

---
+++ SwiftUIBasics/SwiftUIBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicsApp.swift
+//  SwiftUIBasics
+//
+//  Created by Diplomado on 08/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
