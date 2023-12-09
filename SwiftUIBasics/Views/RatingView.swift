8e06c46cf9408a207504cfd4c06c752f95656638
Tere <teresitadejesus.dl8@gmail.com>
2023-12-09 13:59:27 -0600

    Ejercicio PlansView

--- SwiftUIBasics/Views/RatingView.swift
+++ SwiftUIBasics/Views/RatingView.swift
@@ -8,8 +8,25 @@
 import SwiftUI
 
 struct RatingView: View {
+    @State private var rating = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            ForEach(1...5, id: \.self) { index in
+               Image(systemName: index <= self.rating ? "star.fill" : "star")
+                   .resizable()
+                   .frame(width: 30, height: 30)
+                   .foregroundColor(.yellow)
+                   .onTapGesture {
+                       if index == self.rating {
+                           self.rating = 0
+                       } else {
+                           self.rating = index
+                       }
+                   }
+           }
+       }
+       .padding()
     }
 }
 
