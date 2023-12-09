d5c9b5e85f523addeb52d97a45041aecc1c58793
Daniel Mayo <jesusdmr97@gmail.com>
2023-12-09 13:17:21 -0600

    Commit Solution start

--- SwiftUIBasics/Views/RatingView.swift
+++ SwiftUIBasics/Views/RatingView.swift
@@ -7,12 +7,23 @@
 
 import SwiftUI
 
-struct RatingView: View {
+
+struct StartRaiting : View {
+    @State private var raiting : Int = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack{
+            ForEach(1..<6){ index in
+                Image(systemName: index <= raiting ? "star.fill" : "star" )
+                    .foregroundStyle(.yellow)
+                .onTapGesture {
+                    raiting = index
+                }
+            }
+        }
     }
 }
-
 #Preview {
-    RatingView()
+    StartRaiting()
 }
+
