25e52040c2bbe08f911fcb79ce69d9ff41ab0c07
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2023-12-09 14:39:37 -0600

    Implement SignUpView with ViewModel

---
+++ SwiftUIBasics/Views/Components/FormTextField.swift
@@ -0,0 +1,39 @@
+//
+//  FormTextField.swift
+//  SwiftUIBasics
+//
+//  Created by Diplomado on 09/12/23.
+//
+
+import SwiftUI
+
+struct FormTextField: View {
+    var name = ""
+    @Binding var value: String
+    var isSecure = false
+
+    var body: some View {
+        VStack {
+            if isSecure {
+                SecureField(name, text: $value)
+                    .font(.system(size: 20, weight: .semibold, design: .rounded))
+                    .padding(.horizontal)
+            } else {
+                TextField(name, text: $value)
+                    .font(.system(size: 20, weight: .semibold, design: .rounded))
+                    .padding(.horizontal)
+            }
+            Divider()
+                .frame(height: 1)
+                .background(Color.lightGrey)
+                .padding(.horizontal)
+        }
+    }
+}
+
+#Preview {
+    VStack(spacing: 20) {
+        FormTextField(name: "Email", value: .constant(""), isSecure: false)
+        FormTextField(name: "Password", value: .constant(""), isSecure: true)
+    }
+}
