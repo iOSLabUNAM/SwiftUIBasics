8d4f798bc8b650dde8c8f7b7cc03ceb04c74f065
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2023-12-09 08:20:32 -0600

    Implement symbols animations

---
+++ SwiftUIBasics/SymbolsAnimations.swift
@@ -0,0 +1,31 @@
+//
+//  SymbolsAnimations.swift
+//  SwiftUIBasics
+//
+//  Created by Luis Ezcurdia on 09/12/23.
+//
+
+import SwiftUI
+
+struct SymbolsAnimations: View {
+    @State private var animate = false
+    
+    var body: some View {
+        VStack {
+            Image(systemName: animate ? "checkmark.circle" : "faceid")
+                .font(.system(size: 100))
+                .symbolRenderingMode(.palette)
+                .symbolEffect(.bounce, value: animate)
+                .contentTransition(.symbolEffect(.replace))
+                .foregroundStyle(Color.indigo, Color.cyan)
+                .onTapGesture {
+                    UIImpactFeedbackGenerator(style: .medium).impactOccurred()
+                    animate.toggle()
+                }
+        }
+    }
+}
+
+#Preview {
+    SymbolsAnimations()
+}
