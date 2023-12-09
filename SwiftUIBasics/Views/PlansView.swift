69dab014674bf2ce6f59b63a4fd53231c0ade93b
Alejandro Silva <alsxilva@gmail.com>
2023-12-09 13:22:46 -0600

    Plans View Implemented

--- SwiftUIBasics/Views/PlansView.swift
+++ SwiftUIBasics/Views/PlansView.swift
@@ -9,7 +9,114 @@ import SwiftUI
 
 struct PlansView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            ZStack{
+                Rectangle()
+                    .fill(.gray)
+                    .frame(width: 350, height: 200)
+                    .cornerRadius(15)
+                Text("Choose Your Plan")
+                    .font(.system(size: 55))
+                    .bold()
+                    .multilineTextAlignment(.center)
+            }
+            HStack{
+                ZStack {
+                    RoundedRectangle(cornerRadius: 25)
+                        .fill(.blue)
+                        .frame(width: 177, height: 177)
+                    VStack {
+                        Text("Basic")
+                            .font(.system(size: 35))
+                            .bold()
+                            .multilineTextAlignment(.center)
+                        Text("$9")
+                            .font(.system(size: 35))
+                            .bold()
+                            .multilineTextAlignment(.center)
+                        Text("per month")
+                            .font(.system(size: 15))
+                            .multilineTextAlignment(.center)
+                    }
+                    
+                }
+                ZStack {
+                    RoundedRectangle(cornerRadius: 25)
+                        .fill(.red)
+                        .frame(width: 177, height: 177)
+                    VStack {
+                        Text("Pro")
+                            .font(.system(size: 35))
+                            .bold()
+                            .multilineTextAlignment(.center)
+                        Text("$19")
+                            .font(.system(size: 35))
+                            .bold()
+                            .multilineTextAlignment(.center)
+                        Text("per month")
+                            .font(.system(size: 15))
+                            .multilineTextAlignment(.center)
+                        
+                        ZStack {
+                            Rectangle()
+                                .fill(.orange)
+                                .frame(width: 150, height: 20)
+                                .offset(x:0, y:29)
+                            Text("nowww")
+                                .font(.system(size: 15))
+                                .foregroundStyle(.white)
+                                .multilineTextAlignment(.center)
+                                .offset(x:0, y:29)
+                        }
+                        
+                    }
+                }
+            }
+            ZStack{
+                Rectangle()
+                    .fill(.gray)
+                    .frame(width: 350, height: 200)
+                    .cornerRadius(15)
+                    .offset(x:0, y:20)
+                VStack{
+                    Image(systemName: "wand.and.stars.inverse")
+                        .font(.system(size: 35))
+                        .bold()
+                        .foregroundStyle(.white)
+                        .multilineTextAlignment(.center)
+                        .offset(x:0, y:20)
+                    Text("Team")
+                        .font(.system(size: 35))
+                        .bold()
+                        .foregroundStyle(.white)
+                        .multilineTextAlignment(.center)
+                        .offset(x:0, y:20)
+                    Text("$299")
+                        .font(.system(size: 35))
+                        .bold()
+                        .foregroundStyle(.white)
+                        .multilineTextAlignment(.center)
+                        .offset(x:0, y:20)
+                    Text("per month")
+                        .font(.system(size: 15))
+                        .bold()
+                        .foregroundStyle(.white)
+                        .multilineTextAlignment(.center)
+                        .offset(x:0, y:20)
+                    ZStack {
+                        Rectangle()
+                            .fill(.orange)
+                            .frame(width: 150, height: 20)
+                            .offset(x:0, y:41)
+                        Text("nowww")
+                            .font(.system(size: 15))
+                            .foregroundStyle(.white)
+                            .multilineTextAlignment(.center)
+                            .offset(x:0, y:41)
+                    }
+                }
+            }
+        }
     }
 }
 
