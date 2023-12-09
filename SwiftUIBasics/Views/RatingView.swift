d6f56c00b9dd230031a93264fa9d54fa4e1ee901
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2023-12-09 14:42:27 -0600

    Feature: Implement ratting star view

--- SwiftUIBasics/Views/RatingView.swift
+++ SwiftUIBasics/Views/RatingView.swift
@@ -8,8 +8,22 @@
 import SwiftUI
 
 struct RatingView: View {
+    @State var rating: Int = 0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack{
+            ForEach((1..<6)) { idx in
+                Image(systemName: (rating >= idx) ? "star.fill": "star")
+                    .font(.title)
+                    .foregroundColor(.yellow)
+                    .onTapGesture {
+                        if rating == idx {
+                            rating = 0
+                        } else {
+                            rating = idx
+                        }
+                    }
+            }
+        }
     }
 }
 
