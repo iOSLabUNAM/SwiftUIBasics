015d55ec18784478beefd6c56e3d329d66a53f3c
Eric Rojas Pech <ericrokerz@gmail.com>
2023-12-11 01:06:15 -0600

    Added the exersice: PlansView and RatingView

--- SwiftUIBasics/Views/PlansView.swift
+++ SwiftUIBasics/Views/PlansView.swift
@@ -7,12 +7,93 @@
 
 import SwiftUI
 
-struct PlansView: View {
+struct ContentView2: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment:.center){
+            Text("Choose")
+                .font(.system(size: 32, weight: .black, design: .serif))
+                .fontDesign(.rounded)
+            Text("Your Plan")
+                .font(.system(size: 32, weight: .black, design: .serif))
+                .fontDesign(.rounded)
+            HStack {
+                CardView(iconName: "", title: "Basic", description: "Per month", price: "$9")
+                    .foregroundStyle(.white)
+                    .background(.purple)
+                    .cornerRadius(6.0)
+                ZStack {
+                    CardView(iconName: "", title: "Pro", description: "Per month", price: "$19")
+                        .background(.gray)
+                        .cornerRadius(6.0)
+                    BadgeView(text: "Best for designer", padding: 5)
+                        .foregroundStyle(.white)
+                        .background(.orange)
+                        .padding(.top, 200)
+                }
+            }
+            
+            ZStack {
+                CardView(iconName: "sparkles", title: "Team", description:     "                    Per month                   ", price: "299")
+                    .background(.black)
+                    .foregroundStyle(.white)
+                    .cornerRadius(6.0)
+                BadgeView(text: "Perfect for teams with 20 memebers", padding: 5)
+                    .foregroundStyle(.white)
+                    .background(.orange)
+                    .padding(.top, 250)
+
+            }
+        }
+        
+        
     }
 }
 
+
 #Preview {
-    PlansView()
+    ContentView2()
+}
+
+struct CardView: View {
+    var iconName: String
+    var title: String
+    var description: String
+    var price: String
+    
+    var body: some View {
+        VStack(spacing: 10) {
+            Image(systemName: iconName)
+                .font(.system(size: 40))
+                .padding(.top, 30)
+                .padding(.bottom, -15)
+            VStack(spacing: 10) {
+                Text(title)
+                    .font(.system(.title, design: .rounded))
+                    .fontWeight(.black)
+                    .padding(.horizontal)
+                    .padding(.vertical)
+                    .padding(.bottom, -25)
+                Text(price)
+                    .fontWeight(.bold)
+                    .font(.system(size: 40, design: .rounded))
+                Text(description)
+                    .padding(.horizontal,40)
+                    .padding(.vertical)
+                    .padding(.bottom)
+            }
+        }
+    }
+        
+}
+
+struct BadgeView: View {
+    var text: String
+    var padding: CGFloat
+    
+    
+    var body: some View {
+        Text(text)
+            .padding(padding)
+            
+    }
 }
