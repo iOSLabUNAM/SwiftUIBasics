198eef3b21bdec9428f1b6453db9517406859adc
Alan Serralde <serralde.alan@gmail.com>
2024-01-11 12:13:47 -0600

    Add ex

--- SwiftUIBasics/Views/PlansView.swift
+++ SwiftUIBasics/Views/PlansView.swift
@@ -7,12 +7,69 @@
 
 import SwiftUI
 
+struct Plans: View{
+    let hasImg: Bool
+    let imgName: String
+    let planName: String
+    let planCost: Double
+    let hasDesc: Bool
+    let description: String
+    let backColor: Color
+    let fontColor: Color
+    let sizesBack: CGSize
+    var body: some View{
+        ZStack{
+            RoundedRectangle(cornerSize: CGSize(width: 20, height: 20))
+                .frame(width: sizesBack.width, height: sizesBack.height)
+                .aspectRatio(contentMode: .fill)
+                .foregroundStyle(backColor)
+            VStack{
+                if hasImg{
+                    Image(systemName: imgName)
+                    
+                        .font(.largeTitle)
+                }
+                Text(planName)
+                    .font(.title.bold())
+                    .foregroundStyle(fontColor)
+                Text("$\(planCost)")
+                    .font(.title.bold())
+                    .foregroundStyle(fontColor)
+                Text("per month")
+                    .font(.footnote)
+                    .foregroundStyle(fontColor)
+                if hasDesc{
+                    Text(description)
+                        .font(.footnote)
+                        .padding(5)
+                        .foregroundStyle(.white)
+                        .background(.orange)
+                    
+                }
+            }
+        }
+    }
+}
+
 struct PlansView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            
+        Text("Choose \n ur plan 🐥")
+            .font(.title.bold())
+            .foregroundStyle(.black)
+            
+        VStack{
+            HStack{
+                Plans(hasImg: false, imgName: "", planName: "Basic", planCost: 9.9, hasDesc: false, description: "", backColor: .purple, fontColor: .white, sizesBack: CGSize(width: 177, height: 177))
+                Plans(hasImg: false, imgName: "", planName: "Pro", planCost: 19, hasDesc: true, description: "Best for designer", backColor: .gray, fontColor: .black, sizesBack: CGSize(width: 177, height: 177))
+            }
+            
+            Plans(hasImg: true, imgName: "wand.and.stars", planName: "Team", planCost: 299, hasDesc: true, description: "Perfect for teams with 20 members", backColor: .gray, fontColor: .white, sizesBack: CGSize(width: 300, height: 177))
+        }
     }
 }
 
 #Preview {
     PlansView()
 }
+
