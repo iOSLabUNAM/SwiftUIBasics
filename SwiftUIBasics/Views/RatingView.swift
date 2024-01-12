235047ca5d5f5a0a2b79281979789e753f2cb93a
Ramon-OC <ramon.o@ciencias.unam.mx>
2024-01-12 16:42:07 -0600

    Add Rating View

--- SwiftUIBasics/Views/RatingView.swift
+++ SwiftUIBasics/Views/RatingView.swift
@@ -1,17 +1,32 @@
-//
-//  RatingView.swift
-//  SwiftUIBasics
-//
-//  Created by Diplomado on 09/12/23.
-//
+
 
 import SwiftUI
 
-struct RatingView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+ struct RatingView: View {
+     @State private var rating = 0
+     private var starColor = Color(hue: 0.1333, saturation: 0.94, brightness: 0.97)
+     var body: some View {
+         VStack {
+             
+             HStack{
+                 ForEach (1...5, id: \.self) { index in
+                     Image(systemName: index <= self.rating ? "star.fill" : "star")
+                         .resizable()
+                         .frame(width: 35, height: 35)
+                         .foregroundStyle(starColor)
+                         .onTapGesture {
+                             if index == self.rating {
+                                 self.rating = 0
+                             } else {
+                                 self.rating = index
+                             }
+                         }
+                 }
+             }
+         }
+         .padding()
+     }
+ }
 
 #Preview {
     RatingView()
