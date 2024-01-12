c65b288dd635fb2904e1e0b716c1757cad6b1a21
Uriel Soubran <uriel.soubran@gmail.com>
2024-01-12 17:22:24 -0600

    Rating star view - Uriel Soubran

--- SwiftUIBasics/Views/RatingView.swift
+++ SwiftUIBasics/Views/RatingView.swift
@@ -8,8 +8,30 @@
 import SwiftUI
 
 struct RatingView: View {
+    @State private var rating = 0
+    private var starColor = Color(hue: 0.1333, saturation: 0.94, brightness: 0.97)
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Rating")
+                .font(.title)
+                .fontWeight(.bold)
+            HStack{
+                ForEach (1...5, id: \.self) { index in
+                    Image(systemName: index <= self.rating ? "star.fill" : "star")
+                        .resizable()
+                        .frame(width: 35, height: 35)
+                        .foregroundStyle(starColor)
+                        .onTapGesture {
+                            if index == self.rating {
+                                self.rating = 0 //Reset rating to 0 stars if click on the same star
+                            } else {
+                                self.rating = index
+                            }
+                        }
+                }
+            }
+        }
+        .padding()
     }
 }
 
