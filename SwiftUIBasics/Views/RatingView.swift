fe0eb381f53b79f162f17865912e02169613c161
Uriel Soubran <uriel.soubran@gmail.com>
2024-01-12 17:18:18 -0600

    Revert "Rating star view - Uriel Soubran"
    This reverts commit 4fe409bbe79800e00947445e5d1bfd8fa798f76d.

--- SwiftUIBasics/Views/RatingView.swift
+++ SwiftUIBasics/Views/RatingView.swift
@@ -8,30 +8,8 @@
 import SwiftUI
 
 struct RatingView: View {
-    @State private var rating = 0
-    private var starColor = Color(hue: 0.1333, saturation: 0.94, brightness: 0.97)
     var body: some View {
-        VStack {
-            Text("Rating")
-                .font(.title)
-                .fontWeight(.bold)
-            HStack{
-                ForEach (1...5, id: \.self) { index in
-                    Image(systemName: index <= self.rating ? "star.fill" : "star")
-                        .resizable()
-                        .frame(width: 35, height: 35)
-                        .foregroundStyle(starColor)
-                        .onTapGesture {
-                            if index == self.rating {
-                                self.rating = 0 //Reset rating to 0 stars if click on the same star
-                            } else {
-                                self.rating = index
-                            }
-                        }
-                }
-            }
-        }
-        .padding()
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
