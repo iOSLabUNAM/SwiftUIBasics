8b0f75b797bf9cfc721534e884814a480d769a11
eduardoquintero117 <eduardoquiju@hotmail.com>
2024-01-13 10:22:55 -0600

    feature Rating Star System

--- SwiftUIBasics/Views/RatingView.swift
+++ SwiftUIBasics/Views/RatingView.swift
@@ -8,8 +8,26 @@
 import SwiftUI
 
 struct RatingView: View {
+    @State var selected = 0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Rating Star")
+            .padding()
+        
+        HStack{
+            ForEach(0..<5){i in
+                Image(systemName: i <= selected ? "star.fill":"star")
+                    .resizable()
+                    .foregroundStyle(i <= selected ? .yellow:.black)
+                    .frame(width: 40,height: 40).onTapGesture {
+                        selected = i
+                        
+                    }
+            }
+            
+            
+        }
+        
+        
     }
 }
 
