3add73395c7d7ce227fccb9adb09b2a9f4fb5767
Cristian romero <cristian10romero@gmail.com>
2024-01-12 19:59:07 -0600

    completeIssue1

--- SwiftUIBasics/Views/RatingView.swift
+++ SwiftUIBasics/Views/RatingView.swift
@@ -8,8 +8,58 @@
 import SwiftUI
 
 struct RatingView: View {
+    @State var isPressedOne = false
+    @State var isPressedTwo = false
+    @State var isPressedThree = false
+    @State var isPressedFour = false
+    @State var isPressedFive = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack{
+            Button{
+                isPressedOne.toggle()
+            }label: {
+                
+                Image(systemName: isPressedOne ? "star.fill" : "star")
+                    .font(.largeTitle)
+            }
+            Button{
+                isPressedOne.toggle()
+                isPressedTwo.toggle()
+                
+            }label: {
+                
+                Image(systemName: isPressedTwo ? "star.fill" : "star")
+                    .font(.largeTitle)
+            }
+            Button{
+                isPressedOne.toggle()
+                isPressedTwo.toggle()
+                isPressedThree.toggle()
+            }label: {
+                Image(systemName: isPressedThree ? "star.fill" : "star")
+                    .font(.largeTitle)
+            }
+            Button{
+                isPressedOne.toggle()
+                isPressedTwo.toggle()
+                isPressedThree.toggle()
+                isPressedFour.toggle()
+            }label: {
+                Image(systemName: isPressedFour ? "star.fill" : "star")
+                    .font(.largeTitle)
+            }
+            Button{
+                isPressedOne.toggle()
+                isPressedTwo.toggle()
+                isPressedThree.toggle()
+                isPressedFour.toggle()
+                isPressedFive.toggle()
+            }label: {
+                Image(systemName: isPressedFive ? "star.fill" : "star")
+                    .font(.largeTitle)
+            }
+
+        }
     }
 }
 
