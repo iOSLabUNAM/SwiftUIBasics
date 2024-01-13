557490868955523a188004ab6a106319a8ddfcf5
Tere <teresitadejesus.dl8@gmail.com>
2024-01-12 20:07:54 -0600

    Ejercicio SignUpView

--- SwiftUIBasics/Views/Components/FormTextField.swift
+++ SwiftUIBasics/Views/Components/FormTextField.swift
@@ -5,12 +5,14 @@
 //  Created by Diplomado on 09/12/23.
 //
 
+
 import SwiftUI
 
 struct FormTextField: View {
     var name = ""
     @Binding var value: String
     var isSecure = false
+    var fieldType: UIKeyboardType = .default
 
     var body: some View {
         VStack {
@@ -20,6 +22,7 @@ struct FormTextField: View {
                     .padding(.horizontal)
             } else {
                 TextField(name, text: $value)
+                    .keyboardType(fieldType)
                     .font(.system(size: 20, weight: .semibold, design: .rounded))
                     .padding(.horizontal)
             }
@@ -33,7 +36,7 @@ struct FormTextField: View {
 
 #Preview {
     VStack(spacing: 20) {
-        FormTextField(name: "Email", value: .constant(""), isSecure: false)
+        FormTextField(name: "Email", value: .constant(""), isSecure: false, fieldType: .emailAddress)
         FormTextField(name: "Password", value: .constant(""), isSecure: true)
     }
 }
