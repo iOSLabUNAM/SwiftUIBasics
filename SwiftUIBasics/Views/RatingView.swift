68f7c015ab2f02c550fce05606c018d64b84daeb
Ana Paula Flores <anapaulaflores@MacBook-Air-Pau.local>
2024-01-25 14:37:01 -0600

    ej star

--- SwiftUIBasics/Views/RatingView.swift
+++ SwiftUIBasics/Views/RatingView.swift
@@ -1,11 +1,9 @@
 import SwiftUI
 struct StarRatingView: View {
     @State private var rating: Int = 0
-
     var body: some View {
         VStack {
             Text("Selected Rating: \(rating)")
-
             HStack {
                 ForEach(1...5, id: \.self) { index in
                     Image(systemName: index <= self.rating ? "star.fill" : "star")
@@ -21,7 +19,6 @@ struct StarRatingView: View {
         .padding()
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         StarRatingView()
