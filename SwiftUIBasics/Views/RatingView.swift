6f48164758669fbfb22e03a7bbf954c8ff877e37
Eric Margay <48745498+ericmargay@users.noreply.github.com>
2024-01-27 12:50:32 -0600

    Update RatingView.swift

--- SwiftUIBasics/Views/RatingView.swift
+++ SwiftUIBasics/Views/RatingView.swift
@@ -23,8 +23,6 @@ struct RatingView: View {
     }
 }
 
-struct RatingView_Previews: PreviewProvider {
-    static var previews: some View {
-        RatingView()
-    }
+#Preview {
+    RatingView()
 }
