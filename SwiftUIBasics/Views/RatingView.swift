daa768b2ef0894cb707f250207c910e0368666cd
Eric Margay <48745498+ericmargay@users.noreply.github.com>
2024-01-27 12:37:22 -0600

    Update RatingView.swift
    Modifying to a simple solution.

--- SwiftUIBasics/Views/RatingView.swift
+++ SwiftUIBasics/Views/RatingView.swift
@@ -8,27 +8,23 @@
 import SwiftUI
 
 struct RatingView: View {
-    struct Star: Identifiable {
-        let id: Int
-        init(id: Int) {
-            self.id = id
-        }
-    }
-    private var stars = Array(1...5).map { Star(id: $0) }
+    private let maxStars = 5
     @State private var starCount: Int = 0
     
     var body: some View {
         HStack {
-            ForEach(stars) {star in
-                Image(systemName: star.id <= starCount ? "star.fill" : "star")
+            ForEach(1...maxStars, id: \.self) { star in
+                Image(systemName: star <= starCount ? "star.fill" : "star")
                     .onTapGesture {
-                        self.starCount = self.starCount == star.id ? 0 : star.id
+                        self.starCount = self.starCount == star ? 0 : star
                     }
             }
         }
     }
 }
 
-#Preview {
-    RatingView()
+struct RatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        RatingView()
+    }
 }
