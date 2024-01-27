d4271c5fa23f82c6f60f0cb9e665903137df5326
Ramón Ortiz Castañeda <77619575+Ramon-OC@users.noreply.github.com>
2024-01-27 12:33:12 -0600

    Fix EX01

--- SwiftUIBasics/Views/RatingView.swift
+++ SwiftUIBasics/Views/RatingView.swift
@@ -8,10 +8,34 @@
 import SwiftUI
 
 struct RatingView: View {
+    @State private var rating = 0
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                ForEach(1...5, id: \.self) { index in
+                    starImage(for: index)
+                        .onTapGesture {
+                            handleTap(for: index)
+                        }
+                }
+            }
+        }
+        .padding()
+    }
+
+    private func starImage(for index: Int) -> some View {
+        Image(systemName: index <= rating ? "star.fill" : "star")
+            .resizable()
+            .frame(width: 35, height: 35)
+            .foregroundStyle(Color("starColor"))
+    }
+
+    private func handleTap(for index: Int) {
+        rating = (index == rating) ? 0 : index
     }
 }
+
 
 #Preview {
     RatingView()
